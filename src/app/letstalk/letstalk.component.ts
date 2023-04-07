e89f4b2b414180873f06e24ab7ac6fd4ed8a9558
averysmith65 <avery@ngparts.com>
2023-04-07 12:56:17 -0500

    installed youtube iframe and fixed calendly

--- src/app/letstalk/letstalk.component.ts
+++ src/app/letstalk/letstalk.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+declare let Calendly: any;
 
 @Component({
   selector: 'app-letstalk',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./letstalk.component.css']
 })
 export class LetstalkComponent {
+  
+  // @ViewChild('container') container: ElementRef;
 
+  //   ngOnChanges() {
+  //     Calendly.initInlineWidget({
+  //       url: 'https://calendly.com/footballfanalystsnetwork',
+  //       parentElement: this.container.nativeElement
+  //     });
+  // }
 }
